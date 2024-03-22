6f5496c8482f3c23f7ba951d07a2d54b9a0b8bed
Braden <birdnerd2b@gmail.com>
2024-03-22 10:45:28 -0400

    Create Largest-Palindrome-Product.py
    I dont care to make this look pretty

---
+++ Largest-Palindrome-Product.py
@@ -0,0 +1,19 @@
+def isPalindrome(number):
+	num = str(number)
+	palindrome = True
+	for i in range(len(num)-1):
+		j = len(num)-i-1
+		if(num[i] == num[j] and palindrome is True):
+			palindrome = True
+		else:
+			palindrome = False
+			return palindrome
+	return palindrome
+
+upTo, highestPalindrome = int(input("Enter upper boundary: ")), 0
+for i in range(upTo + 1):
+	for j in range(upTo + 1):
+		if(isPalindrome(i * j) is True and (i * j) > highestPalindrome):
+			highestPalindrome = i * j
+
+print(highestPalindrome)
