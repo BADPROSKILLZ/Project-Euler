7a7fa8228dccad05836f60147b2f7145d27a95fc
Braden <birdnerd2b@gmail.com>
2024-05-12 21:27:40 -0400

    Added Answer to Number-letter-Counts.py

--- Number-Letter-Counts.py
+++ Number-Letter-Counts.py
@@ -1,5 +1,6 @@
 """
 Problem 17: If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used? 
+Answer: 21124
 
 Do not count spaces or hyphens. For example, 323 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. 
 The use of "and" when writing out numbers is in compliance with British usage.
