69a58af00557f3280b69b9edbe009657778cb619
Braden <birdnerd2b@gmail.com>
2024-06-03 17:06:40 -0400

    works as intended

--- Counting-Sundays.py
+++ Counting-Sundays.py
@@ -10,10 +10,11 @@ Problem 19: You are given the following information:
     * A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 
 How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+Answer: 171
 """
 def numDaysInMonth(month: int = 1, year: int = 1900) -> int:
     leapYear = False
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):                                                                                                                                                                                                                                                                   
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         leapYear = True
 
     if month in [1, 3, 5, 7, 8, 10, 12]:
@@ -28,17 +29,21 @@ def numDaysInMonth(month: int = 1, year: int = 1900) -> int:
 
 def firstOfTheMonth(month: int, year: int) -> str:
     firstDay = 1 #Monday
-    for yr in range(1900, year+1):
-        for mnth in range(1, month):
-            firstDay += numDaysInMonth(mnth, yr) % 7 #Weekday increased
-            firstDay = (firstDay % 7 if firstDay != 7 else 7) #Fixes number being greater than 7
+    for yr in range(1900, year+1): #Finding the day from Monday, 1/1/1900
+        for mnth in range(1, 13): #Does all months
+            if(yr == year and mnth >= month): break #Limit to stop at the month entered by user
+                #print(firstDay, end="->")
+            firstDay += numDaysInMonth(mnth, yr) % 7 #Add the change in days per month
+                #print(firstDay, end="->")
+            firstDay = ((firstDay % 7) if (firstDay % 7 != 0) else 7) #keep the day within the bounds of 1-7
+                #print(firstDay, "\n")
     
     match firstDay:
         case 1:
             return "Monday"
         case 2:
             return "Tuesday"
-        case 3:
+        case 3: 
             return "Wednesday"
         case 4:
             return "Thursday"
@@ -51,12 +56,10 @@ def firstOfTheMonth(month: int, year: int) -> str:
         case _:
             pass
 
-
 numSundays = 0
-for yr in range(1901, 2001):
-    for mnth in range(1, 13):
-        if firstOfTheMonth(mnth, yr) == "Sunday":
+for year in range(1901, 2001):
+    for month in range (1, 13):
+        if(firstOfTheMonth(month, year) == "Sunday"):
             numSundays += 1
-        
-print(numSundays) #NOT YET WORKING
-            
+
+print(numSundays)
